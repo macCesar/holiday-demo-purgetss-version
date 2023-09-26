3719c5c3036fbd787a5b3f3ed6c30d2b466fc839
Cesar Estrada <cesar@codigomovil.mx>
2023-09-26 10:38:10 -0500

    add alloyXL library

--- app/alloy.js
+++ app/alloy.js
@@ -1,3 +1,5 @@
+require('alloyxl')
+
 Alloy.Collections.content = new Backbone.Collection()
 Alloy.Collections.popular = new Backbone.Collection()
 Alloy.Collections.recomended = new Backbone.Collection()
